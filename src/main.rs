d50adff9840a3db437dd6df93142195c5405c574
nkitan <ankitdas2k@gmail.com>
2025-01-21 01:52:17 +0530

    PERF | Use Pre-serialized JSON for OpenAPI spec

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,13 @@ use routes::root::get_root;
 use std::sync::Arc;
 use routes::users::get_user;
 
-// Wrap the `OpenApi` document in an `Arc` to avoid cloning on each request.
-async fn serve_api(Extension(api): Extension<OpenApi>) -> impl IntoApiResponse {
-    Json(api)
+async fn serve_api(Extension(api_json): Extension<Arc<String>>) -> impl IntoApiResponse {
+    Json((*api_json).clone())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
     let config = Arc::new(ConfigState::from_env().await?);
-
-    let app = ApiRouter::new()
-    .api_route("/", get(get_root))
-    .api_route("/users/{id}", get(get_user))
-    .route("/api.json", get(serve_api))
-    .with_state(config.clone());
 
     let mut api = OpenApi {
         info: Info {
@@ -39,15 +32,25 @@ async fn main() -> anyhow::Result<()>{
         ..OpenApi::default()
     };
 
+    let app = ApiRouter::new()
+    .api_route("/", get(get_root))
+    .api_route("/users/{id}", get(get_user))
+    .with_state(config.clone())
+    .route("/api.json", get(serve_api))
+    .finish_api(&mut api);
+
+    // Serialize the OpenApi document to a JSON string
+    let api_json = serde_json::to_string(&api).expect("Failed to serialize OpenAPI document");
+    let shared_api_json = Arc::new(api_json);
+
+
     let bind_url = format!("{}:{}", config.env.hostname, config.env.port);
     let listener = tokio::net::TcpListener::bind(bind_url).await.unwrap();
     axum::serve(
         listener,
         app
-            // Generate the documentation.
-            .finish_api(&mut api)
             // Expose the documentation to the handlers.
-            .layer(Extension(api))
+            .layer(Extension(shared_api_json))
             .into_make_service(),
     )
     .await
